8fe2911bb33fe9beedf580ca8f6c6fb5b5459d49
Naum74 <78209385+Naum74@users.noreply.github.com>
2025-03-30 16:50:56 +0300

    Update ping_script.py

--- ping_script.py
+++ ping_script.py
@@ -9,7 +9,7 @@ def ping_endpoints():
 
     for url in endpoints:
         try:
-            response = requests.get(url, timeout=10)
+            response = requests.get(url, timeout=30)
             logging.info(f"Pinged {url}. Status code: {response.status_code}")
         except Exception as e:
             logging.error(f"Error pinging {url}: {e}")
